24f3c3358413ce9899e6e16c80cf3ae0b1b8b92a
Sam Silver <sasi1851@colorado.edu>
2020-05-12 21:16:53 -0400

    wordcloud

--- src/eda.py
+++ src/eda.py
@@ -2,6 +2,11 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 import re
+from PIL import Image
+from wordcloud import WordCloud, ImageColorGenerator
+import matplotlib.pyplot as plt
+from sklearn.feature_extraction.text import ENGLISH_STOP_WORDS
+
 def remove_hashtags(string):
     return re.sub(r"\#\S+", "", string)
 
@@ -13,6 +18,14 @@ def remove_line_breaks(string):
     
 def clean_whitespace(string):
      return re.sub(r"\s+", " ", string)
+
+def clean_df_column(df, col):
+    df[col] = df[col].apply(remove_hashtags)
+    df[col] = df[col].apply(remove_tagged_users)
+    df[col] = df[col].apply(remove_line_breaks)
+    df[col] = df[col].apply(clean_whitespace)
+    df[col] = df[col].str.strip()
+    return df
 
 def define_axis_style(ax, title, x_label, y_label, legend=False):
     ax.set_title(title, fontsize=18)
@@ -68,6 +81,16 @@ def plot_pos_neg_dist(df, ax, title, outfilepath=None):
         plt.close('all')
     return
 
+def create_word_cloud(text, additional_stop_words, outfilepath):
+    wordcloud = WordCloud(stopwords=additional_stop_words, width=1600, height=800).generate(text)
+    plt.figure( figsize=(20,10), facecolor='k')
+    plt.imshow(wordcloud, interpolation='bilinear')
+    plt.axis("off")
+    plt.tight_layout()
+    plt.savefig(outfilepath, bbox_inches='tight')
+    plt.close('all')
+    return
+
 if __name__ == '__main__':
     plt.style.use("seaborn")
     raw_df = pd.read_csv("data/Tweets.csv")
@@ -88,3 +111,19 @@ if __name__ == '__main__':
 
     fig, ax = plt.subplots(1, 1, figsize=(10, 6))
     group_plot_pos_neg_dist(high_conf_df, ax, title="Tweet Sentiment by Airline in Training Data (Confidence >= 0.5)", groupby_col='airline', outfilepath="images/airline_tweet_sentiment.png")
+
+
+    # create wordclouds
+    clean_data = pd.read_csv("data/Clean_Tweets.csv")
+
+    clean_data = clean_df_column(clean_data, 'text')
+
+    clean_text_pos = clean_data[clean_data['airline_sentiment'] == 'positive']['text']
+    clean_text_neg = clean_data[clean_data['airline_sentiment'] == 'negative']['text']
+    clean_text_neu = clean_data[clean_data['airline_sentiment'] == 'neutral']['text']
+
+    stop_words = ENGLISH_STOP_WORDS.union(["amp", "flight", "flights", "fly", "airline", "airport", "just", "airlineaccount", "otherairlineaccount", "useraccount"])
+
+    create_word_cloud(text=" ".join(clean_text_pos), additional_stop_words=stop_words, outfilepath="images/pos_wordcloud.png")
+    create_word_cloud(text=" ".join(clean_text_neg), additional_stop_words=stop_words, outfilepath="images/neg_wordcloud.png")
+    create_word_cloud(text=" ".join(clean_text_neu), additional_stop_words=stop_words, outfilepath="images/neu_wordcloud.png")
